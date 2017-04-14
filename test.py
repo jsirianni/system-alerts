25343f7e6f08c2f2d6317f096c847547024f7661
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-04-14 15:27:39 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+import email-gen
+
+#
+# Test script for sending emails
+#
+
+recipient = input("recipient: ")
+subject = input("subject: ")
+body = input("subject: ")
+
+email-gen(recipient, subject, body)
